70f28bc9b8188f17cc932528f04bc1d4f255e646
claramariano <claraamariano@gmail.com>
2025-04-07 08:18:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+var paises = ["Alemanha", "Japão", "Estados Unidos", "Egito", "Mexíco", "Brasil", 
+"Canáda", "Argentina", "Espanha", "Portugal", "Itália", "China"]
+var paisEscolhido = parseInt(prompt("Digite um número de 0 a 10 "))
+
+
+document.write ("País escolhido: " + paises[paisEscolhido])
+   
